bcaebadca6b42ea5f2dc8ae2fc7ee43f143d69d1
Paarroo <code_source_0@protonmail.com>
2025-12-10 20:16:15 +0100

    fix: enable SSR support and update provider loading

--- components/providers/Web3Provider.tsx
+++ components/providers/Web3Provider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode } from 'react'
 import { config } from '../../lib/wagmi'
 import { Web3InnerProvider } from './Web3InnerProvider'
 
@@ -9,22 +9,9 @@ interface Web3ProviderProps {
 }
 
 export function Web3Provider({ children }: Web3ProviderProps) {
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  // Côté serveur : rendu normal sans Web3 (évite toutes les erreurs SSR)
-  if (!mounted) {
-    return <>{children}</>
-  }
-
-  // Côté client : rendu avec Web3 chargé dynamiquement
   return (
     <Web3InnerProvider config={config}>
       {children}
     </Web3InnerProvider>
   )
 }
-// Force redeploy
